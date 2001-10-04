18c17a4abf21ff332459aa691b8f517b67989814
dms <dms@c11ca15a-4712-0410-83d8-924469b57eb5>
2001-10-04 14:18:14 +0000

    - fhs changes
    git-svn-id: https://infobot.svn.sourceforge.net/svnroot/infobot/trunk/blootbot@519 c11ca15a-4712-0410-83d8-924469b57eb5

--- scripts/insertDB.pl
+++ scripts/insertDB.pl
@@ -9,7 +9,7 @@ require "src/logger.pl";
 require "src/modules.pl";
 require "src/Factoids/DBCommon.pl";
 
-&loadConfig($bot_misc_dir."/blootbot.config");
+&loadConfig($bot_config_dir."/blootbot.config");
 &loadDBModules();
 
 unless (@_) {
