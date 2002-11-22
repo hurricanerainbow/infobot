b6498f3ff5829e3ac64c535aa463891bc3e501c3
timriker <timriker@c11ca15a-4712-0410-83d8-924469b57eb5>
2002-11-22 02:50:47 +0000

    table edits
    git-svn-id: https://infobot.svn.sourceforge.net/svnroot/infobot/trunk/blootbot@678 c11ca15a-4712-0410-83d8-924469b57eb5

--- setup/freshmeat.sql
+++ setup/freshmeat.sql
@@ -2,7 +2,6 @@ CREATE TABLE freshmeat (
 	projectname_short VARCHAR(64) NOT NULL,
 	latest_version VARCHAR(32) DEFAULT 'none' NOT NULL,
 	license VARCHAR(32),
-	url_deb VARCHAR(128),
 	url_homepage VARCHAR(128),
 	desc_short VARCHAR(96) NOT NULL,
 
