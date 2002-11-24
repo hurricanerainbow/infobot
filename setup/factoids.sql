d1f7712a533b72eb323651a855ecf922d69bd87e
dms <dms@c11ca15a-4712-0410-83d8-924469b57eb5>
2002-11-24 16:32:32 +0000

    - update table to have default values for numeric columns   hrm... should any columns be NULL at all?
    git-svn-id: https://infobot.svn.sourceforge.net/svnroot/infobot/trunk/blootbot@705 c11ca15a-4712-0410-83d8-924469b57eb5

--- setup/factoids.sql
+++ setup/factoids.sql
@@ -1,17 +1,17 @@
 CREATE TABLE factoids (
 	factoid_key VARCHAR(64) NOT NULL,
 
-	requested_by VARCHAR(64),
-	requested_time INT,
-	requested_count SMALLINT UNSIGNED,
+	requested_by VARCHAR(64) NOT NULL,
+	requested_time INT NOT NULL DEFAULT '0',
+	requested_count SMALLINT UNSIGNED NOT NULL DEFAULT '0',
 	created_by VARCHAR(64),
-	created_time INT,
+	created_time INT NOT NULL DEFAULT '0',
 
 	modified_by VARCHAR(192),
-	modified_time INT,
+	modified_time INT NOT NULL DEFAULT '0',
 
 	locked_by VARCHAR(64),
-	locked_time INT,
+	locked_time INT NOT NULL DEFAULT '0',
 
 	factoid_value TEXT NOT NULL,
 
