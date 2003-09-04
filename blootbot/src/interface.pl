1ac3a8468fdcdbb68b7deace607ed9b759d7727c
timriker <timriker@c11ca15a-4712-0410-83d8-924469b57eb5>
2003-09-04 17:14:01 +0000

    move cliloop to CLI ;-)
    git-svn-id: https://infobot.svn.sourceforge.net/svnroot/infobot/trunk@824 c11ca15a-4712-0410-83d8-924469b57eb5

--- blootbot/src/interface.pl
+++ blootbot/src/interface.pl
@@ -13,32 +13,4 @@ sub whatInterface {
     }
 }
 
-sub cliloop {
-    &status("Using CLI...");
-    &status("Now type what you want.");
-
-    $nuh = "local!local\@local";
-    $uh  = "local\@local";
-    $who = "local";
-    $orig{who} = "local";
-    $ident = $param{'ircNick'};
-    $chan = $talkchannel = "_local";
-    $addressed = 1;
-    $msgType = 'public';
-
-    # install libterm-readline-gnu-perl to get history support
-    use Term::ReadLine;
-    $term = new Term::ReadLine 'blootbot';
-    $prompt = "$who> ";
-    #$OUT = $term->OUT || STDOUT;
-    while ( defined ($_ = $term->readline($prompt)) ) {
-	$orig{message} = $_;
-	$message = $_;
-	chomp $message;
-	last if ($message =~ m/^quit$/);
-	$_ = &process() if $message;
-    }
-    &doExit();
-}
-
 1;
