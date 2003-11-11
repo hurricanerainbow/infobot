c9b744e0096b96625c010defefff25835b3bd2a8
timriker <timriker@c11ca15a-4712-0410-83d8-924469b57eb5>
2003-11-11 20:47:38 +0000

    include blootbot itself
    git-svn-id: https://infobot.svn.sourceforge.net/svnroot/infobot/trunk@876 c11ca15a-4712-0410-83d8-924469b57eb5

--- blootbot/scripts/output_stats.sh
+++ blootbot/scripts/output_stats.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
-
-cd src/
-echo -n "DEBUG:  "; grep DEBUG `find -type f`| wc -l
-echo -n "WARN:   "; grep WARN `find -type f` | wc -l
-echo -n "FIXME:  "; grep FIXME `find -type f` | wc -l
-echo -n "status: "; grep status `find -type f` | wc -l
-echo -n "ERROR:  "; grep ERROR `find -type f` | wc -l
-echo -n "TODO:   "; grep TODO `find -type f` | wc -l
+echo -n "DEBUG:  "; grep DEBUG `find blootbot src -type f`| wc -l
+echo -n "WARN:   "; grep WARN `find blootbot src -type f` | wc -l
+echo -n "FIXME:  "; grep FIXME `find blootbot src -type f` | wc -l
+echo -n "status: "; grep status `find blootbot src -type f` | wc -l
+echo -n "ERROR:  "; grep ERROR `find blootbot src -type f` | wc -l
+echo -n "TODO:   "; grep TODO `find blootbot src -type f` | wc -l
