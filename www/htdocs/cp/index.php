be521e9005c376f57d427056ba3c7552f6092bbd
timriker <timriker@c11ca15a-4712-0410-83d8-924469b57eb5>
2005-02-16 18:20:00 +0000

    oops
    git-svn-id: https://infobot.svn.sourceforge.net/svnroot/infobot/trunk@1116 c11ca15a-4712-0410-83d8-924469b57eb5

--- www/htdocs/cp/index.php
+++ www/htdocs/cp/index.php
@@ -8,7 +8,7 @@
 
 <center><font face="arial" size="+1">C O N T R O L . P A N E L</font><p></center>
 <? box_start("Main Functions",".","90%"); ?>
-<br><b>News Functios:</b><br>
+<br><b>News Functions:</b><br>
 <a href="news.php">Add News</a><br>
 <a href="news-delete.php">Delete News</a><p>
 <? box_end(); ?>
