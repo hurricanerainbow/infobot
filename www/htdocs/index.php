079e29c85da6a0bacf67d06917ce05c73634a301
djmcgrath <djmcgrath@c11ca15a-4712-0410-83d8-924469b57eb5>
2007-10-18 17:08:39 +0000

    * Moved old /trunk/www out of trunk
    git-svn-id: https://infobot.svn.sourceforge.net/svnroot/infobot/trunk@1559 c11ca15a-4712-0410-83d8-924469b57eb5

--- www/htdocs/index.php
+++
@@ -1,9 +0,0 @@
-<?
-  require("/home/groups/b/bl/blootbot/htdocs/inc/template.inc");
-  require("/home/groups/b/bl/blootbot/htdocs/inc/mysql.inc");
-  require("/home/groups/b/bl/blootbot/htdocs/inc/news.inc");
-
-  bloot_header("H O M E");
-  show_news();
-  bloot_footer();
-?>
