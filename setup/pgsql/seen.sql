bf1e1b52e4ca3a0049804f64b0ef19371285d6dd
djmcgrath <djmcgrath@c11ca15a-4712-0410-83d8-924469b57eb5>
2009-10-08 13:03:15 +0000

    * Fixed minor typo in postgresql seen.sql (closes bug #2868436)
    git-svn-id: https://infobot.svn.sourceforge.net/svnroot/infobot/trunk@1875 c11ca15a-4712-0410-83d8-924469b57eb5

--- setup/pgsql/seen.sql
+++ setup/pgsql/seen.sql
@@ -1,12 +1,8 @@
 CREATE TABLE seen (
-    nick VARCHAR(20) NOT NULL,
-    "time" numeric NOT NULL,
-    channel VARCHAR(30) NOT NULL,
-    host VARCHAR(80) NOT NULL,
-    message text NOT NULL,
+   nick character varying(20) NOT NULL, 
+   "time" numeric NOT NULL, 
+   channel character varying(30) NOT NULL, 
+   host character varying(80) NOT NULL, 
+   message text NOT NULL, 
+   CONSTRAINT seen_pkey PRIMARY KEY (nick)
 ) WITHOUT OIDS;
-
-REVOKE ALL ON TABLE seen FROM PUBLIC;
-
-ALTER TABLE ONLY seen
-    ADD CONSTRAINT seen_pkey PRIMARY KEY (nick);
