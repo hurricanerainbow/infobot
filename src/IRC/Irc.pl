e3301da55e9dc113b3369105270bb70533c37b21
timriker <timriker@c11ca15a-4712-0410-83d8-924469b57eb5>
2010-03-04 01:18:55 +0000

    utf8 by default
    git-svn-id: https://infobot.svn.sourceforge.net/svnroot/infobot/trunk@1890 c11ca15a-4712-0410-83d8-924469b57eb5

--- src/IRC/Irc.pl
+++ src/IRC/Irc.pl
@@ -19,9 +19,6 @@ use vars qw($notcount $nottime $notsize $msgcount $msgtime $msgsize
 use vars qw($b_blue $ob);
 use vars qw(@ircServers);
 
-#use open ':utf8';
-#use open ':std';
-
 $nickserv = 0;
 
 # It's probably closer to 510, but let's be cautious until we calculate it extensively.
@@ -128,6 +125,9 @@ sub irc {
 		&ERROR('If this is still a problem, please contact the maintainer.');
 	    }
 	    if (defined $conns{$mynick}) {
+		# explicit binmode for socket as "use open" does not seem to work here
+		binmode $conns{$mynick}->{_socket}, ":utf8";
+
 		$conns{$mynick}->maxlinelen($maxlinelen);
 
 		# handler stuff.
